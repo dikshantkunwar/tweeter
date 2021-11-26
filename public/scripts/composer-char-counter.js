4fb416b40ce703d09cef6cf93671989a7571a834
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-26 12:34:34 -0500

    implemented the character counter function

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on('keyup', function() {
+    let newvalue = 140 - this.value.length;
+    $(this).parent().find('.counter').val(newvalue);
+
+    if (newvalue < 0) {
+      $(this).parent().find('.counter').css('color', 'red');
+    } else {
+      $(this).parent().find('.counter').css('color', '#545149');
+    }
+  })
+});
